76b955b23a380d50a81aae6d84e5cdd9fa3823a1
shivam <shivam198237@gmail.com>
2024-02-04 15:18:17 +0530

    changed cors url

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.listen(aaah, () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://gymfreakss.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
